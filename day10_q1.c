fe0de22350a316da8acc6a88f413304db4606872
25116cn117 <25116cn117@glbitm.ac.in>
2026-06-13 22:47:13 +0530

    Add day10_q.c

---
+++ day10_q1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>(j=1)
+int main(){
+    int n=5,i,j;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n-i;j++){
+            printf(" ");
+            for(j=1;j<=2*i-1;j++){
+                printf("*");
+            }
+        }
+    }
+    return 0;
+}
