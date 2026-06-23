c540092d385bb3e3540e7a454d626f5ea6ddb4e0
25116cn117 <25116cn117@glbitm.ac.in>
2026-06-23 18:24:15 +0530

    Add day16_q.c

---
+++ day16_q3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, sum, i, j;
+
+    printf("Enter size: ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+
+    printf("Enter required sum: ");
+    scanf("%d",&sum);
+
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[i]+arr[j]==sum)
+            {
+                printf("Pair found: %d %d",arr[i],arr[j]);
+                return 0;
+            }
+        }
+    }
+
+    printf("No pair found");
+
+    return 0;
+}
